6798ab1e5f853cbcd31037ca3da2229aeb8c2017
David Morton <dpmorton@gmail.com>
2022-11-24 09:34:08 -0500

    Initial Commit

---
+++ M4L4 lesson/M4L4_lessonApp.swift
@@ -0,0 +1,17 @@
+//
+//  M4L4_lessonApp.swift
+//  M4L4 lesson
+//
+//  Created by dpm on 11/24/22.
+//
+
+import SwiftUI
+
+@main
+struct M4L4_lessonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
